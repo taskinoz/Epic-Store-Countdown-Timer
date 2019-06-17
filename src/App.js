83ed47080e08bdb46fed676dfcf72e5b55c3732b
Clubwho <clubwhomc@gmail.com>
2019-06-17 18:35:08 +1000

    Header and footer made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className="light-text">EPIC GAMES STORE EXCLUSIVE</h2>
+        <h1>COUNTDOWN</h1>
       </header>
+      <section id="game-containers">
+        
+      </section>
+      <footer className="App-footer">
+        <p>Made by Taskinoz and Clubwho</p>
+        <p id="trademarks">©2019 Valve Corporation. Steam and the Steam logo are trademarks and/or registered trademarks of Valve Corporation in the U.S. and/or other countries.<br/>
+©2019, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere.<br/>
+©2019 Microsoft Corporation. Xbox and the Xbox "Sphere" Design are trademarks and/or registered trademarks of Microsoft Corporation in the U.S. and/or other countries.<br/>
+Other brands or product names are the trademarks of their respective owners.</p>
+      </footer>
     </div>
   );
 }
