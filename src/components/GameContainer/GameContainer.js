28b1446a3ff820501ebce40f6784d31432ac4d2f
taskinoz <tristan@taskinoz.com>
2019-06-17 19:05:31 +1000

    Added 2 components

---
+++ src/components/GameContainer/GameContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GameContainer extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className="game-image">
+          <img src="" />
+        </div>
+        <div className="game-info">
+
+        </div>
+      </section>
+    )
+  }
+}
