258f0f875ce4d3f28bfae8ec05ddf356ba3a6713
Clubwho <clubwhomc@gmail.com>
2019-06-17 21:22:00 +1000

    Footer credits link to twitter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component{
   render(){
     return (
       <footer className="App-footer">
-        <p>Made by Taskinoz and Clubwho</p>
+        <p>Made by <a href="https://twitter.com/Taskinoz" target="_blank" rel="noopener noreferrer">Taskinoz</a> and <a href="https://twitter.com/Clubwhom" target="_blank" rel="noopener noreferrer">Clubwho</a></p>
         <p id="trademarks">©2019 Valve Corporation. Steam and the Steam logo are trademarks
           and/or registered trademarks of Valve Corporation in the U.S. and/or other countries.<br/>
           ©2019, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo are trademarks
