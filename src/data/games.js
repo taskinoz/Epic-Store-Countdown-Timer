80b5eef6099f38fc6501bba96b610be1ccca1693
taskinoz <tristan@taskinoz.com>
2019-06-17 20:29:54 +1000

    Renamed data and added 2 games

---
+++ src/data/games.js
@@ -0,0 +1,20 @@
+const games = [{
+  name: "Metro Exodus",
+  image: "",
+  epicRelease: new Date(2019,1,15),
+  steamRelease: new Date(2020,1,15),
+  epicLink: "https://www.epicgames.com/store/en-US/product/metro-exodus/home",
+  steamLink: "https://store.steampowered.com/app/412020/Metro_Exodus/",
+  xboxLink: ""
+}],
+[{
+  name: "The Walking Dead: The Final Season",
+  image: "",
+  epicRelease: new Date(2019,0,15),
+  steamRelease: new Date(2020,0,15),
+  epicLink: "https://www.epicgames.com/store/en-US/product/walking-dead-final-season/home",
+  steamLink: "https://store.steampowered.com/app/866800/The_Walking_Dead_The_Final_Season/",
+  xboxLink: ""
+}];
+
+export default games;
