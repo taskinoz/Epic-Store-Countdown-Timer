1f659c7f88b10848d8a861bb0ad035c7f0fa0e9a
taskinoz <tristan@taskinoz.com>
2019-06-17 23:24:31 +1000

    Added container css, store icons and the link props

--- src/components/GameContainer/GameContainer.js
+++ src/components/GameContainer/GameContainer.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
+// CSS
+import "./GameContainer.css";
+
+// Store Icons
+import epicStore from "../../storeicons/epic_games_store.png";
+import steamStore from "../../storeicons/steam.svg";
+import xboxStore from "../../storeicons/xbox_game_pass.png";
+
 export class GameContainer extends React.Component {
   render() {
     return (
-      <section>
+      <section className="game-container">
         <div className="game-image">
           <img src={this.props.image} />
         </div>
         <div className="game-info">
           <h2>{this.props.name}</h2>
+        </div>
+        <div className="availibility-timer">
+
+        </div>
+        <div className="game-links">
+          <div className="epic">
+            <a href={this.props.epicLink}>
+              <img src={epicStore} />
+            </a>
+          </div>
+          <div className="steam">
+            <a href={this.props.steamLink}>
+              <img src={steamStore} />
+            </a>
+          </div>
+          <div className="xbox">
+            <a href={this.props.xboxLink}>
+              <img src={xboxStore} />
+            </a>
+          </div>
         </div>
       </section>
     )
