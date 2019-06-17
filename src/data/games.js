33720ebe29e0f65763c6ed527bc897265b0fd3c6
Clubwho <clubwhomc@gmail.com>
2019-06-17 21:16:43 +1000

    Fixed games data and added outer worlds

--- src/data/games.js
+++ src/data/games.js
@@ -1,20 +1,34 @@
-const games = [{
-  name: "Metro Exodus",
-  image: "",
-  epicRelease: new Date(2019,1,15),
-  steamRelease: new Date(2020,1,15),
-  epicLink: "https://www.epicgames.com/store/en-US/product/metro-exodus/home",
-  steamLink: "https://store.steampowered.com/app/412020/Metro_Exodus/",
-  xboxLink: ""
-}],
-[{
-  name: "The Walking Dead: The Final Season",
-  image: "",
-  epicRelease: new Date(2019,0,15),
-  steamRelease: new Date(2020,0,15),
-  epicLink: "https://www.epicgames.com/store/en-US/product/walking-dead-final-season/home",
-  steamLink: "https://store.steampowered.com/app/866800/The_Walking_Dead_The_Final_Season/",
-  xboxLink: ""
-}];
+const games = [
+	{
+		name: 'Metro Exodus',
+		image: '',
+		epicRelease: new Date(2019, 1, 15),
+		steamRelease: new Date(2020, 1, 15),
+		epicLink: 'https://www.epicgames.com/store/en-US/product/metro-exodus/home',
+		steamLink: 'https://store.steampowered.com/app/412020/Metro_Exodus/',
+		xboxLink: ''
+	},
+	{
+		name: 'The Walking Dead: The Final Season',
+		image: '',
+		epicRelease: new Date(2019, 0, 15),
+		steamRelease: new Date(2020, 0, 15),
+		epicLink:
+			'https://www.epicgames.com/store/en-US/product/walking-dead-final-season/home',
+		steamLink:
+			'https://store.steampowered.com/app/866800/The_Walking_Dead_The_Final_Season/',
+		xboxLink: ''
+	},
+	{
+		name: 'The Outer Worlds',
+		image: '',
+		epicRelease: new Date(2019, 9, 25),
+		steamRelease: new Date(2020, 9, 25),
+		epicLink:
+			'https://www.epicgames.com/store/en-US/product/the-outer-worlds/home',
+		steamLink: 'https://store.steampowered.com/app/578650/The_Outer_Worlds/',
+		xboxLink: ''
+	}
+];
 
 export default games;
