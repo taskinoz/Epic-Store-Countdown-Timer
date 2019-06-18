57a1fd511f3f1dd2299298c2d9ca4fa98dd3d370
Clubwho <clubwhomc@gmail.com>
2019-06-18 16:24:09 +1000

    Convert to function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export class Header extends React.Component{
-	render(){
-		return (
-			<header className="App-header">
-				<h2 className="light-text">EPIC GAMES STORE EXCLUSIVE</h2>
-				<h1>COUNTDOWN</h1>
-			</header>
-		);
-	}
-}
+export const Header = () => {
+	return (
+		<header className="App-header">
+			<h2 className="light-text">EPIC GAMES STORE EXCLUSIVE</h2>
+			<h1>COUNTDOWN</h1>
+		</header>
+	);
+};
