4c792dea8569d2c7488cb739d613d0a6b69788f7
Clubwho <clubwhomc@gmail.com>
2019-06-18 13:04:46 +1000

    Footer now uses the current year as copyright year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-export class Footer extends React.Component{
-	render(){
-		return (
-			<footer className="App-footer">
-				<p>Made by <a href="https://twitter.com/Taskinoz" target="_blank" rel="noopener noreferrer">Taskinoz</a> and <a href="https://twitter.com/Clubwhom" target="_blank" rel="noopener noreferrer">Clubwho</a></p>
-				<p id="trademarks">©2019 Valve Corporation. Steam and the Steam logo are trademarks
+export const Footer = () => {
+	const thisYear = new Date().getFullYear();
+	return (
+		<footer className="App-footer">
+			<p>Made by <a href="https://twitter.com/Taskinoz" target="_blank" rel="noopener noreferrer">Taskinoz</a> and <a href="https://twitter.com/Clubwhom" target="_blank" rel="noopener noreferrer">Clubwho</a></p>
+			<p id="trademarks">©{thisYear} Valve Corporation. Steam and the Steam logo are trademarks
           and/or registered trademarks of Valve Corporation in the U.S. and/or other countries.<br/>
-          ©2019, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo are trademarks
+          ©{thisYear}, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo are trademarks
           or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere.<br/>
-          ©2019 Microsoft Corporation. Xbox and the Xbox &quot;Sphere&quot; Design are trademarks and/or registered
+          ©{thisYear} Microsoft Corporation. Xbox and the Xbox &quot;Sphere&quot; Design are trademarks and/or registered
           trademarks of Microsoft Corporation in the U.S. and/or other countries.<br/>
           Other brands or product names are the trademarks of their respective owners.
-				</p>
-			</footer>
-		);
-	}
-}
+			</p>
+		</footer>
+	);
+};
